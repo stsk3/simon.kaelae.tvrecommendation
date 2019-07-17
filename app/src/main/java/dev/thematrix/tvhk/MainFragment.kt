7b0f312580f6d038cc9af35a2f96e950fd139055
y2kbug <y2kbug@thematrix.dev>
2019-07-17 22:16:55 +0800

    cleanup

--- app/src/main/java/dev/thematrix/tvhk/MainFragment.kt
+++ app/src/main/java/dev/thematrix/tvhk/MainFragment.kt
@@ -1,17 +1,3 @@
-/*
- * Copyright (C) 2017 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
- */
-
 package dev.thematrix.tvhk
 
 import android.content.Intent
@@ -35,12 +21,8 @@ class MainFragment : BrowseFragment() {
     private fun setupUIElements() {
         title = getString(R.string.browse_title)
 
-        // over title
-        headersState = BrowseFragment.HEADERS_ENABLED
-        isHeadersTransitionOnBackEnabled = true
-
-        // set fastLane (or headers) background color
-        brandColor = ContextCompat.getColor(activity, R.color.fastlane_background)
+        headersState = BrowseFragment.HEADERS_HIDDEN
+        isHeadersTransitionOnBackEnabled = false
     }
 
     private fun loadRows() {
@@ -79,9 +61,5 @@ class MainFragment : BrowseFragment() {
                 Toast.makeText(activity, item, Toast.LENGTH_SHORT).show()
             }
         }
-    }
-
-    companion object {
-        private val NUM_COLS = 15
     }
 }
